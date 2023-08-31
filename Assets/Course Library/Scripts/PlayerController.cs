ee5f12b4b706f794fab58580815f7bb8f93e24c3
MicahY21 <Micahyoung2001@gmail.com>
2023-08-30 17:46:52 -0700

    Unit1LessonC_Edit

--- Assets/Course Library/Scripts/PlayerController.cs
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 15.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +18,6 @@ public class PlayerController : MonoBehaviour
         //Vector3.forward (0, 0, 1)
         //(0, 0, 1) * Time.deltaTime = (0, 0, .016) * = (0, 0, .16)
         
-        transform.Translate(Vector3.forward * Time.deltaTime * 12);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
